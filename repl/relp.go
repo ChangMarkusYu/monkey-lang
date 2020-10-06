50ca790356d83cfd515be7bff56ddffdd578f15d
ChangMarkusYu <marcus.yu.56@gmail.com>
2020-10-05 21:09:36 -0500

    push

---
+++ repl/relp.go
@@ -0,0 +1,79 @@
+package repl
+
+import (
+    "bufio"
+    "fmt"
+    "io"
+    "monkey-lang/lexer"
+    "monkey-lang/parser"
+    "monkey-lang/object"
+    // "monkey-lang/evaluator"
+    "monkey-lang/compiler"
+    "monkey-lang/vm"
+)
+
+const PROMPT = ">> "
+
+func Start(in io.Reader, out io.Writer) {
+    scanner := bufio.NewScanner(in)
+    // env := object.NewEnvironment()
+
+    constants := []object.Object{}
+    globals := make([]object.Object, vm.GlobalsSize)
+    symbolTable := compiler.NewSymbolTable()
+    for i, v := range object.Builtins {
+        symbolTable.DefineBuiltin(i, v.Name)
+    }
+
+    for {
+        fmt.Printf(PROMPT)
+        scanned := scanner.Scan()
+        if !scanned {
+            return
+        }
+
+        line := scanner.Text()
+        l := lexer.New(line)
+        p := parser.New(l)
+
+        program := p.ParseProgram()
+        if len(p.Errors()) != 0 {
+            printParserErrors(out, p.Errors())
+            continue
+        }
+
+        /*evaluated := evaluator.Eval(program, env)
+        if evaluated != nil {
+            io.WriteString(out, evaluated.Inspect())
+            io.WriteString(out, "\n")
+        }*/
+
+        comp := compiler.NewWithState(symbolTable, constants)
+        err := comp.Compile(program)
+        if err != nil {
+            fmt.Fprintf(out, "Compilation failed:\n %s\n", err)
+            continue
+        }
+
+        code := comp.Bytecode()
+        constants = code.Constants
+
+        machine := vm.NewWithGlobalsStore(code, globals)
+        err = machine.Run()
+        if err != nil {
+            fmt.Fprintf(out, "Executing bytecode failed:\n %s\n", err)
+            continue
+        }
+
+        lastPopped := machine.LastPoppedStackElem()
+        io.WriteString(out, lastPopped.Inspect())
+        io.WriteString(out, "\n")
+    }
+}
+
+func printParserErrors(out io.Writer, errors []string) {
+    io.WriteString(out, "parser errors:\n")
+    for _, msg := range errors {
+        io.WriteString(out, "\t" + msg + "\n")
+    }
+}
